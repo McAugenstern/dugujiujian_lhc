6ff1ba49bdf4968bf09d3cb51ef5d94c2db409d3
zengwei <493057693@qq.com>
2019-12-17 16:44:01 +0800

    McAugenstern提交

---
+++ src/main/java/com/atguigu/jianfa/dugujiuidan/Newjianfa.java
@@ -0,0 +1,12 @@
+package com.atguigu.jianfa.dugujiuidan;
+
+/**
+ * @author kitty
+ * @version 1.0
+ * @date 2019/12/17/017 16:42
+ */
+public class Newjianfa {
+    public static void main(String[] args) {
+        System.out.println("新剑法");
+    }
+}
